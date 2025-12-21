3cf9b3fd46f7f7e71e57a46af264045bffd5eb70
Your Name <your-email@example.com>
2025-12-21 15:29:07 +0700

    makeMapBackground

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -8,10 +8,8 @@ export default function RootLayout() {
     <div className="min-h-screen w-full bg-gray-200 flex flex-col">
       <TopBar title="OceanGuard Eye"  logoSrc="/logo_white.png" />
 
-      <main className="flex-1 w-full">
-        <div className="w-full px-[4vw] sm:px-6 lg:px-8 py-4">
-          <Outlet />
-        </div>
+      <main className="flex-1 w-full overflow-hidden">
+        <Outlet />
       </main>
 
       <BottomBar
